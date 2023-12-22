f979165ce87c68d7f7bf44f04d698767147bc201
Platinem <joonaj8@gmail.com>
2023-12-22 20:33:53 +0200

    sprint
    yes

--- BongoHead/Assets/Scripts/Movement.cs
+++ BongoHead/Assets/Scripts/Movement.cs
@@ -5,16 +5,33 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     public float speed = 7.0f;
+    public float sprintMultiplier;
+    private float initialSpeed;
+    public PlayerInput playerInput;
     private Vector2 movementInput;
     private Vector2 smoothedMovementInput;
     private Vector2 movementInputSmoothVelocity;
 
     public new Rigidbody2D rigidbody;
     public Animator animator;
-    // Start is called before the first frame update
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
+    }
+
+    private void Update()
+    {
+        if (playerInput.actions["Sprint"].IsPressed())
+        {
+            speed = initialSpeed * sprintMultiplier;
+        }
+
+        else
+        {
+            speed = initialSpeed;
+        }
     }
 
     private void FixedUpdate()
